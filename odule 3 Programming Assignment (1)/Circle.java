284ec219f1f4da86f2fe20d36910ff64a4f241ee
ipushtoprod <frosted.mini.yeets@gmail.com>
2025-04-07 21:30:08 -0400

    Create Circle.java

---
+++ odule 3 Programming Assignment (1)/Circle.java
@@ -0,0 +1,59 @@
+public class Circle extends GeometricObject implements Comparable<Circle> {
+
+    private double radius;
+    
+    //constructors
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+    //getters/setters
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    // prints the circle
+    public void printCircle() {
+        System.out.println("The circle is created " + getDateCreated() +
+        " and the radius is " + radius);
+    }
+
+    //comparing logic
+    @Override
+    public int compareTo(Circle other) {
+        return Double.compare(this.radius, other.radius);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Circle circle = (Circle) obj;
+        return Double.compare(circle.radius, radius) == 0;
+    }
+}
