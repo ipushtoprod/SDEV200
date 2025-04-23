d47b70cbb044a75ca628a5ad322fce3ee56fb670
ipushtoprod <frosted.mini.yeets@gmail.com>
2025-04-22 21:31:59 -0400

    Update placeholder.java

--- Module 5 Programming Assignment (1)/placeholder.java
+++ Module 5 Programming Assignment (1)/placeholder.java
@@ -1,1 +1,7 @@
-Sorry this is going to be a placeholder for now, I swear I can not get JavaFX to work correctly at all, it keeps throwing every error under the sun and just wont recognize the lib files. Once I get it fixed I'll reupload these.
+Sorry this is going to be a placeholder for now, I swear I can not get JavaFX to work correctly at all, it keeps throwing every error under the sun and just wont recognize the lib files.
+For some reason even when I have my javafx dependencies in the lib folder it is still throwing errors about how javafx is not resolved or something
+tried modifying the launch.json file to include a path the a javafx download but that didnt do anything
+tried using intellij instead and same issue
+redownloading java and javafx versions did nothing
+it must be some random thing that Im just not seeing
+Once I get it fixed I'll reupload these. Sorry.
