46d7419f7f7673d203c437db0f8428770bc1a242
ipushtoprod <frosted.mini.yeets@gmail.com>
2025-05-11 01:48:34 -0400

    Update Main.java

--- Final Project/FX/src/Main.java
+++ Final Project/FX/src/Main.java
@@ -1,12 +1,16 @@
-import java.util.concurrent.ThreadLocalRandom;
 import javafx.application.Application;
+
 import javafx.geometry.Pos;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main extends Application {
 
@@ -23,6 +27,12 @@ public class Main extends Application {
         BorderPane mainContainer = new BorderPane();
         mainContainer.setPrefSize(1000, 1000);
 
+        //instructions for the game
+        Label instructions = new Label(" Guide: \n The top board is the enemy side, the bottom board is your side. \n To start, you must place your own ships on the bottom board. \n Left click will place a ship from left to right, Right click will place a ship from top to bottom. \n The game will start when all 5 ships are placed.");
+        instructions.setAlignment(Pos.CENTER);
+        mainContainer.setTop(instructions);
+
+        //creating the initial grid for the opponent AI, some logic for turns and victory/loss conditions
         opponentGrid = new Grid(true, event -> {
             if (!gameActive)
                 return;
@@ -41,13 +51,14 @@ public class Main extends Application {
             if (opponentsTurn)
                 executeOpponentMove();
         });
-
+    
+        //see above, but for the player
         userGrid = new Grid(false, event -> {
             if (gameActive)
                 return;
 
             Grid.Tile tile = (Grid.Tile) event.getSource();
-            if (userGrid.positionShip(new Ship(shipsToPosition, event.getButton() == MouseButton.PRIMARY), tile.col, tile.row)) {
+            if (userGrid.placeShip(new Ship(shipsToPosition, event.getButton() == MouseButton.PRIMARY), tile.col, tile.row)) {
                 if (--shipsToPosition == 0) {
                     initiateGame();
                 }
@@ -62,14 +73,16 @@ public class Main extends Application {
         return mainContainer;
     }
 
+    // executes when the player makes a move. opponent will immediately make a move after you make a move, more logic
     private void executeOpponentMove() {
         while (opponentsTurn) {
-            int col = randomGenerator.nextInt(12);
-            int row = randomGenerator.nextInt(12);
+            int col = randomGenerator.nextInt(10);
+            int row = randomGenerator.nextInt(10);
 
             Grid.Tile tile = userGrid.getTile(col, row);
-            if (tile.isHit)
+            if (tile.isHit) {
                 continue;
+            }
 
             opponentsTurn = tile.fireAt();
 
@@ -80,27 +93,27 @@ public class Main extends Application {
         }
     }
 
+    // starting the game, creates random ship locations for the opponent, very brute forced
     private void initiateGame() {
-        int shipType = 5;
+        int shipInstance = 5;
 
-        while (shipType > 0) {
-            int col = randomGenerator.nextInt(12);
-            int row = randomGenerator.nextInt(12);
+        while (shipInstance > 0) {
+            int col = randomGenerator.nextInt(10);
+            int row = randomGenerator.nextInt(10);
 
-            if (opponentGrid.positionShip(new Ship(shipType, randomGenerator.nextDouble() < 0.5), col, row)) {
-                shipType--;
+            boolean horizontal = randomGenerator.nextDouble() < 0.5;
+            if (opponentGrid.placeShip(new Ship(shipInstance, horizontal), col, row)) {
+                shipInstance--;
             }
         }
-
         gameActive = true;
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         Scene scene = new Scene(buildInterface());
         primaryStage.setTitle("Battleship");
         primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
